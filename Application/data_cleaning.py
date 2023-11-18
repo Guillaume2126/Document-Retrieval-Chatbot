b559e61cb2fa790c546edefda5838429ebd74f5c
Guillaume Bretel <guillaumebretel@gmail.com>
2023-11-19 00:30:45 +0100

    Making the dataframe

--- Application/data_cleaning.py
+++ Application/data_cleaning.py
@@ -6,7 +6,9 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import PyPDF2
 
+
 def cleaning(sentence):
+    """Explain function"""
     # Basic cleaning
     sentence = re.sub(r'<.*?>', '', sentence)
     sentence = re.sub(r'[^a-zA-Z\s]', '', sentence)  # Remove numbers and characters
@@ -30,17 +32,32 @@ def cleaning(sentence):
     return cleaned_sentence
 
 def extract_text_from_pdf(pdf_path):
+    """Explain function"""
     with open(pdf_path, 'rb') as file:
         pdf_reader = PyPDF2.PdfReader(file)
         text = ""
-        for page_number in range(len(pdf_reader.pages)):
-            text += pdf_reader.pages[page_number].extract_text()
-    return text
+        num_pages = len(pdf_reader.pages)
 
-def process_pdfs_in_directory(directory_path):
+        for page_number in range(num_pages):
+            if page_number < num_pages:
+                text += pdf_reader.pages[page_number].extract_text()
+
+        return text
+
+def process_pdfs_and_build_dictionary(directory_path):
+    """Explain function"""
+    document_texts = {}
+    i=0
+
     for filename in os.listdir(directory_path):
         if filename.endswith(".pdf"):
             pdf_path = os.path.join(directory_path, filename)
             pdf_text = extract_text_from_pdf(pdf_path)
             cleaned_text = cleaning(pdf_text)
-            print(f"Cleaned Text for {pdf_path}:\n{cleaned_text}\n")
+
+            # Add the filename and cleaned text in a dictionnary
+            document_texts[filename] = cleaned_text
+            print(i)
+            i += 1
+
+    return document_texts
